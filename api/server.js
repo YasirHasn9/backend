226e1112c28498eb405cef923f8562a2a62f6d27
YasirHasn9 <hassanyasir223@gmail.com>
2020-05-26 16:20:34 -0700

    create find model for the users-router

--- api/server.js
+++ api/server.js
@@ -4,10 +4,13 @@ const cors = require("cors");
 
 const server = express();
 
+const userRouter = require("../users/users-router");
+
 server.use(express.json());
 server.use(helmet());
 server.use(cors());
 
+server.use("/api/users", userRouter);
 server.get("/api", (req, res) => {
   res.json({ message: "Up" });
 });
