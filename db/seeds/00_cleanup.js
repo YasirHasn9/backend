970bb56ad885958207f252d217ad1edd57b80530
YasirHasn9 <hassanyasir223@gmail.com>
2020-05-27 14:57:09 -0700

    hot fix

--- db/seeds/00_cleanup.js
+++ db/seeds/00_cleanup.js
@@ -1,5 +1,5 @@
 exports.seed = async function(knex) {
-  await knex("user_song").truncate();
-  await knex("songs").truncate();
-  await knex("users").truncate();
+  await knex("user_song").del();
+  await knex("songs").del();
+  await knex("users").del();
 };
