d7fc803751d01d4a24047f219375215226249cbe
YasirHasn9 <hassanyasir223@gmail.com>
2020-05-27 13:49:11 -0700

    run seeds

--- db/seeds/03_userSongs.js
+++ db/seeds/03_userSongs.js
@@ -1,5 +1,10 @@
 exports.seed = async function(knex) {
   await knex("user_song").insert([
-    {  user_id: 1, song_id: 1 }
+    {  user_id: 1, song_id: 1 },
+    {  user_id: 1, song_id: 2 },
+    {  user_id: 2, song_id: 1 },
+    {  user_id: 2, song_id: 2 },
+    {  user_id: 3, song_id: 1 },
+    {  user_id: 3, song_id: 2 },
   ]);
 };
