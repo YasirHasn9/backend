893ee74acc17d1f2c7c5ef87c8924819c3800ebc
YasirHasn9 <hassanyasir223@gmail.com>
2020-05-28 13:42:36 -0700

    configure cors

--- api/server.js
+++ api/server.js
@@ -11,7 +11,13 @@ const restrictedAuth = require("../middleware/restrictedAuth");
 
 server.use(express.json());
 server.use(helmet());
-server.use(cors());
+server.use(
+  cors({
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    preflightContinue: false
+  })
+);
 server.use(cookieParser());
 
 server.use("/api/auth", authRouter);
